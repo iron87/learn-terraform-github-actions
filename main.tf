034f1cf19a3565bb89675056ada338ff3a71f292
iron87 <fede.cerruto@gmail.com>
2021-06-08 14:26:39 +0200

    test

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ terraform {
   backend "remote" {
     organization = "for-demo-purpose-only"
 
-    workspaces {
+    #workspaces {
       #name = "aws-github-actions"
-      prefix = "aws-"
-    }
+      #prefix = "aws-"
+    #}
   }
 }
 
